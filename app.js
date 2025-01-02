a879c99e43110cfd87706ab00900edd9981dd145
d-E-v-E-d <119310921+d-E-v-E-d@users.noreply.github.com>
2025-01-02 15:50:45 +0100

    adding categories + filters adding more pics to galery

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
 // Default arrays
-const defaultBackgrounds = [
-    { name: "pic1.jpg", url: "assets/images/pic1.jpg" },
-    { name: "pic2.jpg", url: "assets/images/pic2.jpg" },
-    { name: "pic3.jpg", url: "assets/images/pic3.jpg" },
-    { name: "pic4.jpg", url: "assets/images/pic4.jpg" },
-    { name: "pic5.jpg", url: "assets/images/pic5.jpg" },
-    { name: "pic6.jpg", url: "assets/images/pic6.jpg" },
-    { name: "pic7.jpg", url: "assets/images/pic7.jpg" },
-    { name: "pic8.jpg", url: "assets/images/pic8.jpg" },
-    { name: "pic9.jpg", url: "assets/images/pic9.jpg" },
-    { name: "pic10.jpg", url: "assets/images/pic10.jpg" },
-    { name: "pic11.jpg", url: "assets/images/pic11.jpg" },
-    { name: "pic12.jpg", url: "assets/images/pic12.jpg" },
-    { name: "pic13.jpg", url: "assets/images/pic13.jpg" },
-    { name: "pic14.jpg", url: "assets/images/pic14.jpg" },
-    { name: "pic15.jpg", url: "assets/images/pic15.jpg" },
-    { name: "pic16.jpg", url: "assets/images/pic16.jpg" },
-    { name: "pic17.jpg", url: "assets/images/pic17.jpg" },
-    { name: "pic18.jpg", url: "assets/images/pic18.jpg" },
-    { name: "pic19.jpg", url: "assets/images/pic19.jpg" },
-    { name: "pic20.jpg", url: "assets/images/pic20.jpg" },
-    { name: "pic21.jpg", url: "assets/images/pic21.jpg" },
-    { name: "pic22.jpg", url: "assets/images/pic22.jpg" },
-    { name: "pic23.jpg", url: "assets/images/pic23.jpg" },
-    { name: "pic24.jpg", url: "assets/images/pic24.jpg" },
-    { name: "pic25.jpg", url: "assets/images/pic25.jpg" },
-    { name: "pic26.jpg", url: "assets/images/pic26.jpg" },
-    { name: "pic27.png", url: "assets/images/pic27.png" },
-    { name: "pic28.jpg", url: "assets/images/pic28.jpg" },
-    { name: "pic29.jpg", url: "assets/images/pic29.jpg" }
+const allBackgrounds = [
+    { name: "pic1.jpg", url: "assets/images/pic1.jpg", category: "wassie" },
+    { name: "pic2.jpg", url: "assets/images/pic2.jpg", category: "wassie" },
+    { name: "pic3.jpg", url: "assets/images/pic3.jpg", category: "wassie" },
+    { name: "pic4.jpg", url: "assets/images/pic4.jpg", category: "wassie" },
+    { name: "pic5.jpg", url: "assets/images/pic5.jpg", category: "wassie" },
+    { name: "pic6.jpg", url: "assets/images/pic6.jpg", category: "wassie" },
+    { name: "pic7.jpg", url: "assets/images/pic7.jpg", category: "wassie" },
+    { name: "pic8.jpg", url: "assets/images/pic8.jpg", category: "wassie" },
+    { name: "pic9.jpg", url: "assets/images/pic9.jpg", category: "wassie" },
+    { name: "pic10.jpg", url: "assets/images/pic10.jpg", category: "wassie" },
+    { name: "pic11.jpg", url: "assets/images/pic11.jpg", category: "wassie" },
+    { name: "pic12.jpg", url: "assets/images/pic12.jpg", category: "wassie" },
+    { name: "pic13.jpg", url: "assets/images/pic13.jpg", category: "wassie" },
+    { name: "pic14.jpg", url: "assets/images/pic14.jpg", category: "wassie" },
+    { name: "pic15.jpg", url: "assets/images/pic15.jpg", category: "wassie" },
+    { name: "pic16.jpg", url: "assets/images/pic16.jpg", category: "wassie" },
+    { name: "pic17.jpg", url: "assets/images/pic17.jpg", category: "wassie" },
+    { name: "pic18.jpg", url: "assets/images/pic18.jpg", category: "wassie" },
+    { name: "pic19.jpg", url: "assets/images/pic19.jpg", category: "wassie" },
+    { name: "pic20.jpg", url: "assets/images/pic20.jpg", category: "wassie" },
+    { name: "pic21.jpg", url: "assets/images/pic21.jpg", category: "wassie" },
+    { name: "pic22.jpg", url: "assets/images/pic22.jpg", category: "wassie" },
+    { name: "pic23.jpg", url: "assets/images/pic23.jpg", category: "wassie" },
+    { name: "pic24.jpg", url: "assets/images/pic24.jpg", category: "wassie" },
+    { name: "pic25.jpg", url: "assets/images/pic25.jpg", category: "wassie" },
+    { name: "pic26.jpg", url: "assets/images/pic26.jpg", category: "wassie" },
+    { name: "pic27.png", url: "assets/images/pic27.png", category: "wassie" },
+    { name: "pic28.jpg", url: "assets/images/pic28.jpg", category: "wassie" },
+    { name: "pic29.jpg", url: "assets/images/pic29.jpg", category: "wassie" },
+    { name: "gen1.jpg", url: "assets/images/gen1.jpg", category: "general" },
+    { name: "gen2.jpg", url: "assets/images/gen2.jpg", category: "general" },
+    { name: "gen3.jpg", url: "assets/images/gen3.jpg", category: "general" },
+    { name: "gen4.jpg", url: "assets/images/gen4.jpg", category: "general" },
+    { name: "gen5.jpg", url: "assets/images/gen5.jpg", category: "general" },
+    { name: "gen6.jpg", url: "assets/images/gen6.jpg", category: "general" },
+    { name: "gen7.jpg", url: "assets/images/gen7.jpg", category: "general" },
+    { name: "gen8.jpg", url: "assets/images/gen8.jpg", category: "general" },
+    { name: "gen9.jpg", url: "assets/images/gen9.jpg", category: "general" },
+    { name: "gen10.jpg", url: "assets/images/gen10.jpg", category: "general" },
+    { name: "gen11.jpg", url: "assets/images/gen11.jpg", category: "general" },
+    { name: "gen12.jpg", url: "assets/images/gen12.jpg", category: "general" },
+    { name: "gen13.jpg", url: "assets/images/gen13.jpg", category: "general" },
+    { name: "gen14.jpg", url: "assets/images/gen14.jpg", category: "general" },
+    { name: "gen15.jpg", url: "assets/images/gen15.jpg", category: "general" },
+    { name: "gen16.jpg", url: "assets/images/gen16.jpg", category: "general" },
+    { name: "gen17.jpg", url: "assets/images/gen17.jpg", category: "general" },
+    { name: "gen18.jpg", url: "assets/images/gen18.jpg", category: "general" },
+    { name: "gen19.jpg", url: "assets/images/gen19.jpg", category: "general" },
+    { name: "gen20.jpg", url: "assets/images/gen20.jpg", category: "general" },
+    { name: "gen21.jpg", url: "assets/images/gen21.jpg", category: "general" },
+    { name: "gen22.jpg", url: "assets/images/gen22.jpg", category: "general" },
+    { name: "gen23.jpg", url: "assets/images/gen23.jpg", category: "general" },
+    { name: "gen24.jpg", url: "assets/images/gen24.jpg", category: "general" },
+    { name: "gen25.jpg", url: "assets/images/gen25.jpg", category: "general" },
+    { name: "gen26.jpg", url: "assets/images/gen26.jpg", category: "general" },
+    { name: "gen27.png", url: "assets/images/gen27.png", category: "general" },
+    { name: "gen28.jpg", url: "assets/images/gen28.jpg", category: "general" },
+    { name: "gen29.jpg", url: "assets/images/gen29.jpg", category: "general" }
 ];
+
 
 const defaultStickers = [
     { name: "sticker1.png", url: "assets/stickers/sticker1.png" },
@@ -51,6 +81,7 @@ let canvas;
 let selectedObject = null;
 
 document.addEventListener('DOMContentLoaded', () => {
+
     // 1) Initialize Fabric canvas
     canvas = new fabric.Canvas('meme-canvas', {
         preserveObjectStacking: true,
@@ -60,16 +91,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // 2) Initialize tabs
     initTabs();
 
-    // 3) Add default backgrounds and stickers to the galleries
-    defaultBackgrounds.forEach(bg => {
-        addToGallery({
-            url: bg.url,
-            galleryId: "background-gallery",
-            onclickFunction: `selectBackground('${bg.url}')`,
-            altText: bg.name
-        });
-    });
+    // 3) Populate background gallery with all items initially
+    //    We'll do the filtering by calling a function if needed.
+    populateBackgroundGallery(allBackgrounds);
 
+    //   Populate sticker gallery
     defaultStickers.forEach(sticker => {
         addToGallery({
             url: sticker.url,
@@ -89,6 +115,92 @@ document.addEventListener('DOMContentLoaded', () => {
         selectedObject = null;
     });
 });
+function filterBackgrounds() {
+    const select = document.getElementById('bg-category-select');
+    const category = select.value; // 'all', 'empty', or 'wassie'
+
+    let filtered = [];
+    if (category === 'all') {
+        filtered = allBackgrounds;
+    } else {
+        filtered = allBackgrounds.filter(bg => bg.category === category);
+    }
+
+    // Repopulate the gallery
+    populateBackgroundGallery(filtered);
+}
+
+function populateBackgroundGallery(backgroundArray) {
+    const gallery = document.getElementById('background-gallery');
+    gallery.innerHTML = ''; // clear existing
+
+    backgroundArray.forEach(bg => {
+        addToGallery({
+            url: bg.url,
+            galleryId: 'background-gallery',
+            onclickFunction: `selectBackground('${bg.url}')`,
+            altText: bg.name
+        });
+    });
+}
+
+////////////////////////////
+// ADD TO GALLERY UTILITY //
+////////////////////////////
+function addToGallery({ url, galleryId, onclickFunction, altText }) {
+    const gallery = document.getElementById(galleryId);
+    if (!gallery) return;
+
+    const img = document.createElement('img');
+    img.src = url;
+    img.alt = altText || 'Image';
+    img.style.width = '120px';
+    img.style.cursor = 'pointer';
+    img.setAttribute('onclick', onclickFunction);
+
+    gallery.appendChild(img);
+}
+
+////////////////////
+// FILE UPLOADS   //
+////////////////////
+function initUploads() {
+    const bgUploadInput = document.getElementById('uploadBackgroundImages');
+    const stickerUploadInput = document.getElementById('uploadStickers');
+
+    bgUploadInput.addEventListener('change', (e) => {
+        const files = e.target.files;
+        if (!files || !files.length) return;
+
+        for (const file of files) {
+            const imageUrl = URL.createObjectURL(file);
+            addToGallery({
+                url: imageUrl,
+                galleryId: 'background-gallery',
+                onclickFunction: `selectBackground('${imageUrl}')`,
+                altText: file.name
+            });
+        }
+        e.target.value = '';
+    });
+
+    stickerUploadInput.addEventListener('change', (e) => {
+        const files = e.target.files;
+        if (!files || !files.length) return;
+
+        for (const file of files) {
+            const stickerUrl = URL.createObjectURL(file);
+            addToGallery({
+                url: stickerUrl,
+                galleryId: 'sticker-gallery',
+                onclickFunction: `addSticker('${stickerUrl}')`,
+                altText: file.name
+            });
+        }
+        e.target.value = '';
+    });
+}
+
 
 // -------------------------
 // SELECTION HANDLER
